c81d7c78d94a138266112c4f9f5107f80f2fd551
pradyumn12299 <pradyumn1276@gmail.com>
2024-03-23 12:55:32 +0530

    third time chnages

--- src/FisrtProgram.java
+++ src/FisrtProgram.java
@@ -1,8 +1,5 @@
 public class FisrtProgram {
     public static void main(String[] args) {
         System.out.println("hello world");
-        int a=10,b=20;
-        int c=a+b;
-        System.out.println(c);
     }
 }
--- src/SecondProgram.java
+++ src/SecondProgram.java
@@ -1,5 +1,9 @@
 public class SecondProgram {
     public static void main(String[] args) {
-        System.out.println("helllo world");
+        //performing addition
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("Addition of two numbers are: "+ c);
     }
 }
