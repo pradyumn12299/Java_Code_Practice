4400b65bcdcd015642e2e496e0fcf85cd8c66e19
pradyumn12299 <pradyumn1276@gmail.com>
2024-03-23 12:51:06 +0530

    second commit

--- src/FisrtProgram.java
+++ src/FisrtProgram.java
@@ -1,5 +1,8 @@
 public class FisrtProgram {
     public static void main(String[] args) {
         System.out.println("hello world");
+        int a=10,b=20;
+        int c=a+b;
+        System.out.println(c);
     }
 }
